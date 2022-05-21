c0f1b36963083573fdb7c5296e6d86a293251966
Хабибуллин Руслан <sabibah2002@gmail.com>
2022-05-22 01:57:12 +0600

    Добавил запись с микрофона

--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -4,6 +4,13 @@ public static class Program
 {
     public static void Main()
     {
+        var recorder = new Recorder("test2.wav");
+        Console.WriteLine("Начать запись?");
+        Console.Read();
+        recorder.Start();
+        Console.WriteLine("Нажмите Enter чтобы остановить запись...");
+        Console.ReadKey();
+        recorder.Stop();
         Converter.ToTxt("test.wav");
         var result = File.ReadAllText("../../../test.txt");
         Console.WriteLine(result);
---
+++ Receiver/Recorder.cs
@@ -0,0 +1,59 @@
+﻿using CSCore;
+using CSCore.Codecs.WAV;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.Streams;
+
+namespace Receiver;
+
+public class Recorder
+{
+    private string _output;
+    private WasapiCapture _capture;
+    private IWaveSource _source;
+    private WaveWriter _writer;
+
+    public Recorder(string filename = "output.wav")
+    {
+        this._output = filename;
+        this._capture = new WasapiCapture();
+        this._capture.Device = FindMicrophone();;
+        this._capture.Initialize();
+    }
+    
+    public void Start()
+    {
+        var soundInSource = new SoundInSource(this._capture);
+        var singleBlockNotificationStream = new SingleBlockNotificationStream(soundInSource.ToSampleSource());
+        this._source = singleBlockNotificationStream.ToWaveSource();
+    
+        this._writer = new WaveWriter("../../../" + this._output, this._source.WaveFormat);
+    
+        var buffer = new byte[this._source.WaveFormat.BytesPerSecond / 2];
+        soundInSource.DataAvailable += (s, e) =>
+        {
+            int read;
+            while ((read = this._source.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                this._writer.Write(buffer, 0, read);
+            }
+        };
+        
+        this._capture.Start();
+    }
+
+    public void Stop()
+    {
+        this._capture.Stop();
+        this._capture.Dispose();
+        this._source.Dispose();
+        this._writer.Dispose();
+    }
+
+    private static MMDevice FindMicrophone()
+    {
+        var deviceEnumerator = new MMDeviceEnumerator();
+        var deviceCollection = deviceEnumerator.EnumAudioEndpoints(DataFlow.Capture, DeviceState.Active);
+        return deviceCollection[0];
+    }
+}
